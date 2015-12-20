7091bfa960aaaa10f42c89318e00c6888ea53995
Matthew Robinson <robinson.me@gmail.com>
2015-12-20 16:35:12 -0500

    End of week 2
    Multiple users and nicer looking.

--- textcircle.js
+++ textcircle.js
@@ -1,4 +1,5 @@
 this.Documents = new Mongo.Collection("documents");
+EditingUsers = new Mongo.Collection("editingUsers");
 
 if (Meteor.isClient) {
 
@@ -13,14 +14,34 @@ if (Meteor.isClient) {
     },
     config: function(editor) {
       return function (editor) {
+        editor.setOption("lineNumbers", true);
+        editor.setOption("mode", "html");
+        editor.setOption("theme", "isotope");
         editor.on('change', function(cm_editor, info) {
           $("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
-          console.log(cm_editor.getValue());
+          Meteor.call("addEditingUser");
         });
       }
     }
   });
-}
+
+  Template.editingUsers.helpers({
+    users: function() {
+      var doc, eusers, users;
+      doc = Documents.findOne();
+      if (!doc) {return;} // if no doc found give up
+      eusers = EditingUsers.findOne({docid:doc._id});
+      if (!eusers) {return;} // if no users found give up
+      users = new Array();
+      var i = 0;
+      for (var user_id in eusers.users) {
+        users[i] = fixObjectKeys(eusers.users[user_id]);
+        i++;
+      }
+      return users;
+    }
+  });
+} // end isClient
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
@@ -29,4 +50,39 @@ if (Meteor.isServer) {
       Documents.insert({title: "my new document"});
     }
   });
+}
+
+Meteor.methods({
+  addEditingUser:function () {
+    var doc, user, eusers;
+    doc = Documents.findOne();
+    if (!doc) {
+      return; // no doc give up
+    }
+    if (!this.userId) {
+      return; // no logged in user, give up
+    }
+    user = Meteor.user().profile;
+    eusers = EditingUsers.findOne({docid: doc._id});
+    if (!eusers) {
+      eusers = {
+        docid: doc._id,
+        users: {
+
+        }
+      };
+    }
+    user.lastEdit = new Date();
+    eusers.users[this.userId] = user;
+    EditingUsers.upsert({_id: eusers._id}, eusers);
+  }
+})
+
+function fixObjectKeys(obj){
+  var newObj = {};
+  for (key in obj) {
+    var key2 = key.replace("-", "");
+    newObj[key2] = obj[key];
+  }
+  return newObj;
 }
