8e8681b65c1c474bec2b807cbb1276ca8432ba5d
Matthew Robinson <robinson.me@gmail.com>
2015-12-19 22:37:21 -0500

    Week One done
    for Coursera

--- textcircle.js
+++ textcircle.js
@@ -14,8 +14,9 @@ if (Meteor.isClient) {
     config: function(editor) {
       return function (editor) {
         editor.on('change', function(cm_editor, info) {
+          $("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
           console.log(cm_editor.getValue());
-        });        
+        });
       }
     }
   });
